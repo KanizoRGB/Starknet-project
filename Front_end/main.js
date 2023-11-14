8a545acf0d5cf8c00e83c2b5f9641badbc37e180
Kanillar <kanillareugiene2000@gmail.com>
2023-11-14 08:23:07 +0300

    added the front-end

---
+++ Front_end/main.js
@@ -0,0 +1,5 @@
+<script>
+        // Add your JavaScript code here
+        // You would typically use JavaScript to handle user interactions, like login, adding/deleting books, and searching.
+        // You'll also need to make AJAX requests to a backend for data and user authentication.
+</script>
